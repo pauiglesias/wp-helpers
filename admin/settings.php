9371fc5bb3523b5caa3158e8d9e15dd11c490a72
Pau <pauiglesias@gmail.com>
2022-07-22 18:37:30 +0200

    Added the settings page

--- admin/settings.php
+++ admin/settings.php
@@ -2,7 +2,10 @@
 
 namespace MicroDeploy\Package\Admin;
 
+use \MicroDeploy\Package\Helpers\Module;
+use \MicroDeploy\Package\Helpers\Notices;
 use \MicroDeploy\Package\Helpers\Singleton;
+use \MicroDeploy\Package\Helpers\Util;
 
 /**
  * Settings class
@@ -14,13 +17,40 @@ final class Settings extends Singleton {
 
 
 
-
+	/**
+	 * Handle form submit
+	 */
 	public function handle() {
 
+		if (!$this->verifyNonce('nonce_settings')) {
+			return;
+		}
+
+		// Update code or function call ...
+
+		Notices::success('Data successfully updated');
 	}
 
 
 
+	/**
+	 * Verifies submit nonce
+	 */
+	private function verifyNonce($key) {
+
+		if (!wp_verify_nonce(Util::postParam($key), Module::file())) {
+			Notices::error('Not valid security credentials. Please try again.');
+			return false;
+		}
+
+		return true;
+	}
+
+
+
+	/**
+	 * Display page content
+	 */
 	public function display() {
 
 	}
