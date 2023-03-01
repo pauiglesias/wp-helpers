7e1a465b9e8d2e59d5fc5a844f77eff7fa46f361
Pau <pauiglesias@gmail.com>
2023-03-01 20:07:23 +0100

    Updated AJAX class

--- helpers/ajax.php
+++ helpers/ajax.php
@@ -7,6 +7,8 @@ namespace MicroDeploy\Package\Helpers;
  *
  * Wrapper AJAX methods and default response definition.
  *
+ * @uses Module class
+ *
  * @package		WordPress
  * @subpackage	Helpers
  * @version		1.0.0
@@ -19,18 +21,29 @@ class Ajax {
 
 
 	/**
-	 * Checking for a valid nonce
+	 * Checking for a valid ajax nonce
+	 *
+	 * @param string		$param 	The POST param name to verify the nonce (will NOT be prefixed).
+	 * @param string|null	$seed	The optional seed that created the nonce (by default the project FILE constant).
+	 *
+	 * @return int|false	1 if the nonce is valid and generated between 0-12 hours ago,
+	 *						2 if the nonce is valid and generated between 12-24 hours ago.
+	 *						false if the nonce is invalid or the basis WP function is not available.
 	 */
-	public static function verifyNoncePosted($param, $seed) {
+	public static function verifyNoncePosted($param, $seed = null) {
 		return !empty($_POST[$param]) &&
 		 		function_exists('wp_verify_nonce') &&
-				wp_verify_nonce($_POST[$param], $seed);
+				wp_verify_nonce($_POST[$param], isset($seed) ? $seed : Module::file());
 	}
 
 
 
 	/**
-	 * Default response
+	 * Default response, consisting of a status indicator and associated data.
+	 *
+	 * @param array		$data	An array of data for the output response.
+	 *
+	 * @return array The defined response.
 	 */
 	public static function default($data = []) {
 		return ['status' => 'ok', 'data' => $data];
@@ -39,7 +52,11 @@ class Ajax {
 
 
 	/**
-	 * Output with data
+	 * Outputs response on success in JSON format.
+	 *
+	 * @param array		$data	An array of data for the output response.
+	 *
+	 * @return void
 	 */
 	public static function success($data = []) {
 		self::json(self::default($data));
@@ -48,7 +65,12 @@ class Ajax {
 
 
 	/**
-	 * Output an error response
+	 * Outputs an error response in JSON format.
+	 *
+	 * @param string	$reason		The error description.
+	 * @param array		$data		An array of data for the output response.
+	 *
+	 * @return void
 	 */
 	public static function error($reason, $data = []) {
 		self::json(['status' => 'error', 'reason' => $reason, 'data' => $data]);
@@ -57,7 +79,11 @@ class Ajax {
 
 
 	/**
-	 * AJAX response in JSON format
+	 * Ouputs the AJAX response in JSON format
+	 *
+	 * @param array		$response	The response array to be encoded in JSON format.
+	 *
+	 * @return void
 	 */
 	public static function json($response) {
 		@header('Content-Type: application/json');
@@ -68,6 +94,10 @@ class Ajax {
 
 	/**
 	 * Allows to customize the Json flags
+	 *
+	 * @param string|null	$flags	Optional flags to encode.
+	 *
+	 * @return string The default JSON flags or the defined ones.
 	 */
 	public static function jsonFlags($flags = null)  {
 
