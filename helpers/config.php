341cab256cd030de65409eb0289b26f0c8d12600
asimetrica <pauiglesias@gmail.com>
2025-12-11 16:47:44 +0100

    New instance and value method

--- helpers/config.php
+++ helpers/config.php
@@ -15,12 +15,19 @@ namespace MicroDeploy\Package\Helpers;
  *
  * @package		WordPress
  * @subpackage	Helpers
- * @version		1.0.0
+ * @version		1.1.0
  * @license		GPLv3
  * @author		Pau Iglesias
  * @link		https://github.com/pauiglesias/wp-helpers
  */
 class Config {
+
+
+
+	/**
+	 * @var array Stores instances of the Config class indexed by filename and subdirectory.
+	 */
+	private static $instances = [];
 
 
 
@@ -61,6 +68,30 @@ class Config {
 
 
 	/**
+	 * Get or create a Config instance
+	 *
+	 * Returns an existing instance of the Config class for the specified filename
+	 * and subdirectory, or creates a new one if it doesn't exist. This implements
+	 * the Singleton pattern to ensure only one instance exists per unique configuration.
+	 *
+	 * @param string $filename The name of the configuration file. Defaults to 'config.php'.
+	 * @param string $subdirectory The subdirectory where the file is located. Defaults to an empty string.
+	 * @return self The Config instance for the specified filename and subdirectory.
+	 */
+	public static function instance($filename = 'config.php', $subdirectory = '') {
+
+		$key = $filename . '|' . $subdirectory;
+
+		if (!isset(self::$instances[$key])) {
+			self::$instances[$key] = new self($filename, $subdirectory);
+		}
+
+		return self::$instances[$key];
+	}
+
+
+
+	/**
 	 * Prepare subdirectory data
 	 *
 	 * Processes the provided subdirectory path, ensuring it has the correct
@@ -92,6 +123,28 @@ class Config {
 	*/
 	public function data() {
 		return isset($this->data) ? $this->data : $this->load();
+	}
+
+
+
+	/**
+	 * Get a specific value from the configuration data
+	 *
+	 * Retrieves a value from the configuration array by navigating through
+	 * section, key, and optionally subkey levels.
+	 *
+	 * @param string $section The main section of the configuration array.
+	 * @param string|null $key Optional. The key within the section. If null, returns the entire section.
+	 * @param string|null $subkey Optional. The subkey within the key. If null, returns the value at the key level.
+	 * @return mixed The value found at the specified path in the configuration array.
+	 */
+	public function value($section, $key = null, $subkey = null) {
+		return !isset($key)
+			? $this->data()[$section]
+			: (!isset($subkey)
+				? $this->data()[$section][$key]
+				: $this->data()[$section][$key][$subkey]
+			);
 	}
 
 
